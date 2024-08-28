82011c1f112264532c6faac116c00e2e7036b326
NikhilD1998 <nikhildadab@hotmail.com>
2024-08-28 20:09:42 +0530

    Added Responsive Nav Bar

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -3,6 +3,28 @@ import "./navbar.css";
 import logo from "../../assets/logo.png";
 import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
 
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT?</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
+};
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
@@ -12,21 +34,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -50,6 +58,13 @@ const Navbar = () => {
               setToggleMenu(true);
             }}
           />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
         )}
       </div>
     </div>
